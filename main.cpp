d60e23c100af6d25b8c7991f28028bdb916524d8
al-bimani <=>
2020-05-11 18:59:17 +0300

    updated to latest functionality

--- main.cpp
+++ main.cpp
@@ -7,12 +7,17 @@ using namespace std;
 
 int main()
 {
-    Inst inputstream = Inst("var 3.14");
+    Inst inputstream = Inst("var pi = 3.14");
     Tknst tokenstream = Tknst(inputstream);
 
-    Token *ident = tokenstream.readnext();
-    cout << ident->value << endl; // var
-    Token *num = tokenstream.readnext();
-    cout << num->value; // 3.14
+    Token *token = new Token();
+
+    while (!tokenstream.eof())
+    {
+        token = tokenstream.next();
+
+        cout << token->tostring() << endl;
+    }
+
     return 0;
 }
